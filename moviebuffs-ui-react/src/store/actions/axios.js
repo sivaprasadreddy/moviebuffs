2ace9ad216c599e0d64e121dbc25f16a7afedeba
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2020-02-19 20:54:42 +0530

    Fix api module plugin config to bundle ui code.

--- moviebuffs-ui-react/src/store/actions/axios.js
+++ moviebuffs-ui-react/src/store/actions/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import {cleanState, getAccessToken} from "../localStorage";
 
 const instance = axios.create();
-const localApiUrl = "http://localhost:8080";
+//const localApiUrl = "http://localhost:8080";
+const localApiUrl = "";
 
 let apiUrl = process.env.REACT_APP_API_BASE_URL;
 console.log("REACT_APP_API_BASE_URL from env: " + apiUrl);
