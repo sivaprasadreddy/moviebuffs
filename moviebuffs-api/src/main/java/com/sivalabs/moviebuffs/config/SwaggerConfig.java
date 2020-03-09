fa7d4829dd6a35481f973472060bbd063895e0d8
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2020-03-09 09:08:55 +0530

    Clean up unnecessary files.

--- moviebuffs-api/src/main/java/com/sivalabs/moviebuffs/config/SwaggerConfig.java
+++ moviebuffs-api/src/main/java/com/sivalabs/moviebuffs/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.sivalabs.moviebuffs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,13 +16,16 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                 //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.sivalabs.moviebuffs.web.controller"))
+                // .paths(PathSelectors.any())
+                 .paths(PathSelectors.ant("/api/**"))
                 .build()
                 .apiInfo(apiInfo());
     }
